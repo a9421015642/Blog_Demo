0dbba72bb13be5292398ccf6784ce85ed3241461
Ian <a9421015642@gmail.com>
2016-03-09 06:08:02 +0800

    add create : article & category

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,27 @@
 class ArticlesController < ApplicationController
+
   def index
     @categories = Category.all
     @articles = Article.all
   end
+  def new
+    @categories = Category.all
+    @articles = Article.all
+    @article = Article.new
+  end
+  def create
+    article = Article.new(article_params)
+    article.user = current_user
+     if article.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+ private 
+  def  article_params
+    params.require(:article).permit(:title , :content)
+  end
+
 end
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,27 @@
 class CategoriesController < ApplicationController
+
+  def index 
+    @categories = Category.all
+    @articles = Article.all
+  end
+
+  def new 
+    @categories = Category.all
+    @articles = Article.all
+    @category = Category.new
+  end
+  def create
+    @category = Category.new(category_params)
+     if @category.save
+      redirect_to categories_path
+    else
+      render :new
+    end
+  end
+
+ private 
+  def  category_params
+    params.require(:category).permit(:title)
+  end
+
 end
